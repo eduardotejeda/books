08ca8509737fdc0ad4af68bb347ca29c26ca25b2
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-04-18 06:49:30 -0400

    Add delete button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from './components/BookList'
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const deleteBooksById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+
+    };
 
     const createBook = (title) => {
         const updatedBooks = [
@@ -14,8 +24,8 @@ function App() {
 
 
     return (
-    <div>
-        {books.length}
+    <div className="app">
+        <BookList books={books} onDelete={deleteBooksById}/>
         <BookCreate onCreate={createBook}/>
     </div>
     );
