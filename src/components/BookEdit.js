66800f32a1180c555bd6153c82460d3be4e16c1f
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-04-19 06:53:02 -0400

    Copy files from tutorial
    To find out why the fuck onEdit props is not a function

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,29 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function BookEdit({book, onEdit}) {
-    const [title, setTitle] = useState(book.title);
+function BookEdit({ book, onEdit }) {
+  const [title, setTitle] = useState(book.title);
 
-    const handleChange = (event) => {
-        setTitle(event.target.value);
-    };
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        onEdit(book.id, title);
-    };
+    onEdit(book.id, title);
+  };
 
-
-    return (
+  return (
     <form onSubmit={handleSubmit} className="book-edit">
-        <label>Title</label>
-        <input className="input" value={title} onChange={handleChange}/>
-        <button className="button is-primary">
-         Save
-        </button>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
     </form>
-    )
+  );
 }
-
 
 export default BookEdit;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,13 @@
 import BookShow from './BookShow';
 
-function BookList({books, onDelete, onEdit}) {
-    const renderedBooks = books.map((book) => {
-        return (
-        <BookShow ondEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>
-        );
-    })
-    return <div className='book-list'>{renderedBooks}</div>
+function BookList({ books, onDelete, onEdit }) {
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    );
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
